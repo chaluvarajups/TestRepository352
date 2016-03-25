34c80014e4e60dad3390f5fe5e1d240aca495477
crajups <crajups@gmail.com>
2016-03-26 00:07:36 +0530

    fourth commit

--- Learning_Selenium/src/Git/GitCode.java
+++ Learning_Selenium/src/Git/GitCode.java
@@ -6,7 +6,7 @@ public class GitCode {
 	public static void main(String[] args) {
 		System.out.println("GitHUb config- version control");
 		System.out.println("push to remote server");
-		System.out.println("Fetch from hub or remote server")
+		System.out.println("Fetch from hub or remote servr")
 	}
 
 }
