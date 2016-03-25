286ed7cae6ade5cc823e1e9a8df3e16a82f4121c
CrajuPS <crajups@gmail.com>
2016-03-25 23:58:08 +0530

    My second commit

--- Learning_Selenium/src/Git/GitCode.java
+++ Learning_Selenium/src/Git/GitCode.java
@@ -1,10 +1,11 @@
 package Git;
 
+
 public class GitCode {
 
 	public static void main(String[] args) {
 		System.out.println("GitHUb config- version control");
-
+		System.out.println("Fetch changes");
 	}
 
 }
